1e024a08e2dc26698de5970d61857642991c1a9c
Leo Conforti <leo@leoconforti.us>
2025-01-23 18:23:09 +0000

    start

---
+++ vitest.config.ts
@@ -0,0 +1,34 @@
+/// <reference types="vitest" />
+
+import path from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    test: {
+        globals: true,
+        setupFiles: ["./test/setup-file.ts"],
+        globalSetup: "./test/setup-global.ts",
+        include: ["./test/**/*.test.ts"],
+        reporters: ["default"],
+        coverage: {
+            provider: "v8",
+        },
+    },
+    server: {
+        watch: {
+            ignored: [
+                "**/node_modules/**",
+                "**/.git/**",
+                "**/submodules/**",
+                "**/reflection/**",
+                "**/docs/**",
+                "**/examples/**",
+            ],
+        },
+    },
+    resolve: {
+        alias: {
+            "turbotelescope.net": path.resolve(__dirname, "services"),
+        },
+    },
+});
