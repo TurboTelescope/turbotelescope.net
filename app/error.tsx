4aca1cde5edee6d5377f3e34681ba8faf5b89a45
Gram012 <gramo012@umn.edu>
2025-03-20 12:04:45 -0500

    fix error page

--- app/error.tsx
+++ app/error.tsx
@@ -7,9 +7,6 @@ export default function Custom500() {
         <div style={{ textAlign: "center", marginTop: "50px" }}>
             <h1>500 - Internal Server Error</h1>
             <p>Something went wrong. Please try again later.</p>
-            <Link href="/">
-                <a>Go back to Home</a>
-            </Link>
         </div>
     );
 }
